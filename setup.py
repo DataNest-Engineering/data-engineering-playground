0935de8815eb3d9eec33119c68a2a4d965c793ec
ML expert <securemlops@gmail.com>
2025-09-08 09:45:43 +0530

    data engineering repo setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dataplay",
+    version="0.0.1",
+    author="data engineers",
+    author_email="",
+    packages=find_packages()
+)
